e823bd12dc2dda8e1fa11647c581b2d775d23e80
vrishali183 <vrishali183@gmail.com>
2023-12-12 11:11:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,12 +21,12 @@ function gotResults(error, results) {
     random_number_b = Math.floor(Math.random() * 255) + 1;
 
 
-    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
-    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
-    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
-    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
-
-    img = document.getElementById('animal_image');
+    document.getElementById("").innerHTML = 'Detected voice is of  - '+ results[0].label;//add result_label
+    document.getElementById("").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;//result_count
+    document.getElementById("").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";// add result_label
+    document.getElementById("").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";//result_count
+   //add id animal_image
+    img = document.getElementById('');
 
     if (results[0].label == "Barking") {
       img.src = 'bark.gif';
